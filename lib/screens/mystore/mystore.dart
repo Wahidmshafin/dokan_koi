243785eb932ecf9137b93daf9bd8272b6f435419
Shafin <mahmood1807074@stud.kuet.ac.bd>
2022-10-15 17:28:15 +0600

    Mystore added

--- lib/screens/mystore/mystore.dart
+++ lib/screens/mystore/mystore.dart
@@ -8,7 +8,7 @@ class MyStore extends StatelessWidget {
   static String routeName = "/mystore";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Body(),
       bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.mystore),
     );
