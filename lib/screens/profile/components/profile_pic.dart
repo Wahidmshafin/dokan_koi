df085f7f210c64bcaae27f8f3157edcf1c44410c
Shafin <mahmood1807074@stud.kuet.ac.bd>
2022-11-06 23:29:03 +0600

    Profile Picture can be added

--- lib/screens/profile/components/profile_pic.dart
+++ lib/screens/profile/components/profile_pic.dart
@@ -1,8 +1,12 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class ProfilePic extends StatefulWidget {
    ProfilePic({
@@ -15,19 +19,40 @@ class ProfilePic extends StatefulWidget {
 
 class _ProfilePicState extends State<ProfilePic> {
 
-  File? image;
+  String? image;
+  FirebaseStorage storage = FirebaseStorage.instance;
+  FirebaseFirestore cloud = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
 
   Future getImage() async{
     print("hoi na ken");
+    var tmpImage;
     try{
       final tmp=await ImagePicker().pickImage(source: ImageSource.gallery);
       if(tmp==null)
       {
         return;
       }
+
+      try{
+        if(image!=null){
+          await storage.ref("profile/${auth.currentUser?.uid}").delete();
+        }
+
+        await storage.ref("profile/${auth.currentUser?.uid}").putFile(File(tmp.path));
+        tmpImage=await storage.ref("profile/${auth.currentUser?.uid}").getDownloadURL();
+        cloud.collection("profile").doc(auth.currentUser?.uid).update({"image":tmpImage});
+      }
+      on FirebaseException catch(e){
+        print(e.message);
+      }
+
       setState(() {
-        image=File(tmp.path);
+        image=tmpImage;
+        print(image);
       });
+
+
     }
     catch(e){
         print(e);
@@ -36,39 +61,48 @@ class _ProfilePicState extends State<ProfilePic> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 115,
-      width: 115,
-      child: Stack(
-        fit: StackFit.expand,
-        clipBehavior: Clip.none,
-        children: [
-          image==null?
-          const CircleAvatar(
-            backgroundImage: AssetImage("assets/images/user.png"),
-          ): CircleAvatar(backgroundImage: FileImage(image!)),
-          Positioned(
-            right: -16,
-            bottom: 0,
-            child: SizedBox(
-              height: 46,
-              width: 46,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    side: BorderSide(color: Colors.white),
+    return StreamBuilder(
+      stream: cloud.collection("profile").doc(auth.currentUser?.uid).snapshots(),
+      builder: (context, snapshot) {
+        if(snapshot.data?.data()!["image"]!=null){
+          print("ekhane ken ase");
+          image=snapshot.data?.data()!["image"];
+        }
+        return SizedBox(
+          height: 115,
+          width: 115,
+          child: Stack(
+            fit: StackFit.expand,
+            clipBehavior: Clip.none,
+            children: [
+              image==null?
+              const CircleAvatar(
+                backgroundImage: AssetImage("assets/images/user.png"),
+              ): CircleAvatar(backgroundImage: NetworkImage(image!)),
+              Positioned(
+                right: -16,
+                bottom: 0,
+                child: SizedBox(
+                  height: 46,
+                  width: 46,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        side: BorderSide(color: Colors.white),
+                      ),
+                      primary: Colors.white,
+                      backgroundColor: Color(0xFFF5F6F9),
+                    ),
+                    onPressed: getImage,
+                    child: SvgPicture.asset("assets/icons/Camera Icon.svg"),
                   ),
-                  primary: Colors.white,
-                  backgroundColor: Color(0xFFF5F6F9),
                 ),
-                onPressed: getImage,
-                child: SvgPicture.asset("assets/icons/Camera Icon.svg"),
-              ),
-            ),
-          )
-        ],
-      ),
+              )
+            ],
+          ),
+        );
+      }
     );
   }
 }
